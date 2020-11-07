d2c608e2a2897d6e8a93f98f822471cdff519708
MIchael Kahlbacher <kahlbacher.michael@gmail.com>
2020-11-07 17:49:57 +0100

    Initial Commit

---
+++ LocationTimeTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationTimeTracker
+//
+//  Created by Michael Kahlbacher on 07.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
