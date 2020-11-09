1a29f948bda6a26b5c220e4e91d608f93aa20603
MIchael Kahlbacher <kahlbacher.michael@gmail.com>
2020-11-09 20:38:59 +0100

    Project Clean UP, Delete Storyboard, UI will be programmatic

---
+++ LocationTimeTracker/Costum Views/TabBarController/LTTTabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  LTTTabBarController.swift
+//  LocationTimeTracker
+//
+//  Created by Michael Kahlbacher on 09.11.20.
+//
+
+import UIKit
+
+class LTTTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    func createLocationOverviewNC(){
+        //let locationOverviewVC =
+    }
+
+}
