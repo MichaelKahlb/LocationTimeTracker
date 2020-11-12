8aff3ab9ac5ff1c37f288b212329fcc96077497b
MIchael Kahlbacher <kahlbacher.michael@gmail.com>
2020-11-12 21:05:39 +0100

    Basic Project Setup Done. Added Tabbarcontroller and Navigation Controllers.

---
+++ LocationTimeTracker/Utilities/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  LocationTimeTracker
+//
+//  Created by Michael Kahlbacher on 12.11.20.
+//
+
+import UIKit
+
+enum Titles {
+    static let regions = "Regions"
+    static let times = "Times"
+}
+
+
+enum SFSymbols {
+    static let map                 = UIImage(systemName: "map")
+    static let clock               = UIImage(systemName: "clock")
+}
