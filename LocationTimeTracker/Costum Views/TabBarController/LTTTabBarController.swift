975c73968d21263bc5f21b2ed7b25033d1a3f6ce
MIchael Kahlbacher <kahlbacher.michael@gmail.com>
2020-11-12 22:25:12 +0100

    test

--- LocationTimeTracker/Costum Views/TabBarController/LTTTabBarController.swift
+++ LocationTimeTracker/Costum Views/TabBarController/LTTTabBarController.swift
@@ -11,11 +11,22 @@ class LTTTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewControllers = [createTimesNC(), createLocationOverviewNC()]
     }
     
     
-    func createLocationOverviewNC(){
-        //let locationOverviewVC =
+    func createLocationOverviewNC()-> UINavigationController {
+        let regionsVC = RegionsVC()
+        regionsVC.title = Titles.regions
+        regionsVC.tabBarItem = UITabBarItem(title: Titles.regions, image: SFSymbols.map, tag: 1)
+        return UINavigationController(rootViewController: regionsVC)
+    }
+    
+    func createTimesNC()-> UINavigationController {
+        let timesVC = TimesVC()
+        timesVC.title = Titles.times
+        timesVC.tabBarItem = UITabBarItem(title: Titles.times, image: SFSymbols.clock, tag: 1)
+        return UINavigationController(rootViewController: timesVC)
     }
 
 }
