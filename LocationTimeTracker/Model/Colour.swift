0420f5cd5b7a98e0bcd25f5a4a9959b3a98814b9
MIchael Kahlbacher <kahlbacher.michael@gmail.com>
2020-11-22 22:37:18 +0100

    Finished Persistence Manager. Added update Function

---
+++ LocationTimeTracker/Model/Colour.swift
@@ -0,0 +1,19 @@
+//
+//  Colour.swift
+//  LocationTimeTracker
+//
+//  Created by Michael Kahlbacher on 22.11.20.
+//
+
+import UIKit
+
+struct Color: Codable, Hashable {
+    var red: CGFloat
+    var green: CGFloat
+    var blue: CGFloat
+    var alpha: CGFloat?
+
+    var uiColor: UIColor {
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha ?? 1)
+    }
+}
--- LocationTimeTracker/Model/Region.swift
+++ LocationTimeTracker/Model/Region.swift
@@ -6,20 +6,8 @@
 //
 
 import UIKit
-import CoreLocation
 
-struct Color: Codable {
-    var red: CGFloat
-    var green: CGFloat
-    var blue: CGFloat
-    var alpha: CGFloat?
-
-    var uiColor: UIColor {
-        return UIColor(red: red, green: green, blue: blue, alpha: alpha ?? 1)
-    }
-}
-
-struct Region: Codable {
+struct Region: Codable, Hashable {
     let name: String
     let colour: Color
     let radius: Double
