ee87daf99d0ac6e29a9c0bfcf86f36e570bc1b26
MIchael Kahlbacher <kahlbacher.michael@gmail.com>
2020-11-23 20:47:00 +0100

    Added Persistence Manager to load saved Regions

--- LocationTimeTracker/Screens/RegionsVC.swift
+++ LocationTimeTracker/Screens/RegionsVC.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class RegionsVC: UIViewController {
+    
+    let tableView = UITableView()
+    var regions: [Region] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        configureViewController()
+        loadRegions()
+    }
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func loadRegions(){
+        PersistenceManager.retrieveRegions { [weak self] (result) in
+            guard let self = self else { return }
+            switch result {
+            case .success(let regions):
+                self.regions = regions
+            case .failure(let error):
+                print(error.rawValue)
+            }
+        }
     }
 
 }
